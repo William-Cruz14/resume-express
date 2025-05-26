505d983182cc7bc62a9bbaaf54df7887f0a59312
William Cruz <cruzwilliam14@gmail.com>
2025-05-26 11:20:51 -0300

    Debugando fase 3

--- api/config/database.js
+++ api/config/database.js
@@ -2,21 +2,30 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 let sequelize;
 
-if(process.env.DATABASE_URL){
+
+if (process.env.DATABASE_URL) {
 	console.log('Conectando ao banco de dados remoto...');
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
 		dialect: 'postgres',
-		protocol: 'postgres',
 		dialectOptions: {
 			ssl: {
 				require: true,
 				rejectUnauthorized: false,
 			},
 		},
-		logging: process.env.NODE_ENV === 'development' ? console.log : false,
+		logging: false, // Desative logs em produção
 	});
 } else {
-	console.log('Conectando ao banco de dados local...');
+
+	console.warn('DATABASE_URL não encontrada. Tentando conexão local...');
+
+	const requiredVars = ['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_HOST', 'DB_PORT'];
+	const missingVars = requiredVars.filter(varName => !process.env[varName]);
+
+	if (missingVars.length) {
+		throw new Error(`Variáveis de ambiente necessárias não configuradas: ${missingVars.join(', ')}`);
+	}
+
 	sequelize = new Sequelize(
 		process.env.DB_NAME,
 		process.env.DB_USER,
@@ -25,18 +34,19 @@ if(process.env.DATABASE_URL){
 			host: process.env.DB_HOST,
 			port: process.env.DB_PORT,
 			dialect: 'postgres',
-			logging: process.env.NODE_ENV === 'development' ? console.log : false,
+			logging: process.env.NODE_ENV === 'development',
 		}
 	);
-
 }
 
 const testConnection = async () => {
 	try {
 		await sequelize.authenticate();
 		console.log('Conexão com o banco de dados estabelecida com sucesso.');
+		return true;
 	} catch (error) {
 		console.error('Erro ao conectar ao banco de dados:', error);
+		return false;
 	}
 };
 
